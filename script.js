fda25338d18e6023f34e4947a4bb03cbca9a7b48
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2023-09-18 11:29:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	Array.sort(arr);
+	let ans[]={};
+	let i=0;
+	let n=arr.length;
+	let diff=Number.MAX_VALUE;
+	let ans=0;
+	while (i<n) {
+		let j=i+1;
+		let k=n-1;
+		while (j<k) {
+			let sum=arr[i]+arr[j]+arr[k];
+			if(sum==target)
+			{
+				return sum;
+		    }
+			else if(Math.abs(target-sum)<diff)
+			{
+				diff=Math.abs(target-sum);
+				ans=sum;
+			}
+			if(sum<target)
+			{
+				j++;
+				while (j<n && arr[j]==arr[j-1]) {
+					j++;
+				}
+			}
+			else
+			{
+				k--;
+				while (k>=0 && arr[k]==arr[k+1]) {
+					k--;
+				}
+			}
+		}
+		i++;
+	}
+	return ans;
 }
 
 module.exports = threeSum;
